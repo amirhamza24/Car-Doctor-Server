1c879bc83248d35b815a947532ce41b3ef967401
amirhamza24 <ridoykhan7897890@gmail.com>
2023-05-27 20:32:21 +0600

    bookings section update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,11 +28,33 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
     
     app.get('/services', async(req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);   
+    })
+
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+        const options = {
+            projection: { title: 1, price: 1, service_id: 1 },
+        };
+
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+    })
+
+
+    // bookings
+    app.post('/bookings', async(req, res) => {
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
     })
 
 
